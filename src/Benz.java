9b2c2783a77f2ec729d8a649e99d93cf664887d0
nafiseh <fun101.nm@gmail.com>
2020-01-29 13:31:16 +0330

    homework 2-4

---
+++ src/Benz.java
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 10/5/2019.
+ */
+public class Benz {
+    public  void  model(){
+        System.out.println("model 1 model 2 model 3 model 4 model 5");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by admin on 10/5/2019.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        if (a== )
+        String a =input.nextLine();
+        Benz m1 = new Benz();
+        m1.model();
+    }
+}
